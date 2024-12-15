a40879c5043547df37364e5a33914cdf0dad17f2
nadamohmmed0 <n.mohmmed2466@nu.edu.eg>
2024-12-15 20:52:48 +0200

    Add files via upload

---
+++ oop.py
@@ -0,0 +1,23 @@
+#object orinted programming(oop)
+# car  model color type
+# running
+
+# class car:
+#     def __init__(self):اهم حاجه self
+#         print("hello")
+# car()        
+# class car:
+#     # def __init__(self,model,color,type):
+#     #     self.carmodel=model
+#     #     self.carcolor=color
+#     #     self.cartype=type
+        
+#     def run(self):
+#         print("hello world") 
+# car().run()    
+# class person:
+#     def __init__(self,name,major,id):
+#         self.personname=name
+#         self.personmajor=major
+#         self.personid=id
+
